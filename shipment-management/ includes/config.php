81c71946d0bdd43b98f937573958aa9eba7ed654
KIRAN-CREDENT <kankatalakiran.com@gmail.com>
2024-08-27 02:04:25 +0530

    config.php

---
+++ shipment-management/ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+// Database connection details for JsonPowerDB
+$baseUrl = "http://api.login2explore.com:5577"; // base URL
+$dbName = "DELIVERY-DB"; //database name
+$collectionName = "SHIPMENT-TABLE"; //rel name
+?>
